b6591de807dd0b92b8cb7f7867d1959902512c92
Tamás Mikola <mikola.tamas@recrovit.hu>
2024-07-12 17:01:25 +0200

    Enhance RgfGridRequest constructor

--- src/Models/RgfChartParam.cs
+++ src/Models/RgfChartParam.cs
@@ -4,7 +4,7 @@ namespace Recrovit.RecroGridFramework.Abstraction.Models;
 
 public class RgfChartParam
 {
-    public string Aggregate { get; set; } = "Count";
+    public string? Aggregate { get; set; }
 
     public int PropertyId { get; set; } = 0;
 
