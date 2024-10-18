22c2d9600ecbd351665b2888a00b0a5b4b430553
Tamás Mikola <mikola.tamas@recrovit.hu>
2024-10-18 08:40:29 +0200

    Replaced ChartData with Aggregation parameter class

--- src/Models/RgfAggregationSettings.cs
+++ src/Models/RgfAggregationSettings.cs
@@ -3,15 +3,15 @@ using System.Text.Json.Serialization;
 
 namespace Recrovit.RecroGridFramework.Abstraction.Models;
 
-public class RgfChartParam
+public class RgfAggregationSettings
 {
-    public List<RgfGroupColumn> Columns { get; set; } = new List<RgfGroupColumn>();
+    public List<RgfAggregationColumn> Columns { get; set; } = new List<RgfAggregationColumn>();
 
     [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
     public int? Take { get; set; }
 }
 
-public class RgfGroupColumn
+public class RgfAggregationColumn
 {
     [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
     public string Aggregate { get; set; }
