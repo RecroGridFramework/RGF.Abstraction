7bac806af758eb5cd6c2cd47224fa2593135e8d2
Tamás Mikola <mikola.tamas@recrovit.hu>
2024-11-09 18:41:13 +0100

    -Enhanced RgfGridRequest and RgfGridResult classes -Refactored RgfAggregationSettings and RgfChartSetting classes

--- src/Models/RgfAggregationSettings.cs
+++ src/Models/RgfAggregationSettings.cs
@@ -1,23 +1,41 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Text.Json.Serialization;
 
 namespace Recrovit.RecroGridFramework.Abstraction.Models;
 
-public class RgfAggregationSettings
+public class RgfAggregationSettings : ICloneable
 {
     public List<RgfAggregationColumn> Columns { get; set; } = new();
 
-    public List<int> Groups { get; set; } = new ();
+    public List<RgfIdAliasPair> Groups { get; set; } = new();
 
-    public List<int> SubGroup { get; set; } = new();
+    public List<RgfIdAliasPair> SubGroup { get; set; } = new();
+
+    public virtual object Clone()
+    {
+        var clone = new RgfAggregationSettings();
+        clone.Columns = Columns.Select(c => c.Clone() as RgfAggregationColumn).ToList();
+        clone.Groups = Groups.Select(g => g.Clone() as RgfIdAliasPair).ToList();
+        clone.SubGroup = SubGroup.Select(s => s.Clone() as RgfIdAliasPair).ToList();
+        return clone;
+    }
 }
 
-public class RgfAggregationColumn
+public class RgfAggregationColumn : RgfIdAliasPair
 {
-    public static readonly string[] AllowedAggregates = { "Sum", "Avg", "Min", "Max", "Count" };
+    public static readonly string[] AllowedAggregates = { "Sum", "Avg", "Min", "Max", "Count", "-Sum" };
+
+    public RgfAggregationColumn() { }
+
+    public RgfAggregationColumn(RgfAggregationColumn aggregationColumn) : base(aggregationColumn) 
+    {
+        Aggregate = aggregationColumn.Aggregate;
+    }
 
     [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
     public string Aggregate { get; set; }
 
-    public int PropertyId { get; set; } = 0;
+    public override object Clone() => new RgfAggregationColumn(this);
 }
